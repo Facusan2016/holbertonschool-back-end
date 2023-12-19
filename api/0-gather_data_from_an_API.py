e23c0c306f811f35b9694a73d1515d5964da03c1
Facusan2016 <facusan2016@gmail.com>
2023-12-19 16:16:19 -0300

    fix exercise 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -16,10 +16,10 @@ if __name__ == '__main__':
     user_data = requests.get(api_url + f'users/{user_id}').json()
     user_tasks = requests.get(api_url + f'users/{user_id}/todos').json()
 
-    completed_tasks = sum(1 for task in user_tasks if task['completed'])
+    user_completed_tasks = [t for t in user_tasks if t['completed']]
 
     print(f'Employee {user_data["name"]} is done with ', end='')
-    print(f'tasks({completed_tasks}/{len(user_tasks)}):')
+    print(f'tasks({len(user_completed_tasks)}/{len(user_tasks)}):')
 
-    for task in user_tasks:
-        print(f'\t {task["title"]}')
+    for task in user_completed_tasks:
+        print("\t " + task["title"])
