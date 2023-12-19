b22ec168f45dfc9aa63ef983669a974ce58f3fda
Facusan2016 <facusan2016@gmail.com>
2023-12-19 19:28:02 -0300

    exercise 2 added

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+    Program to gather data from an API.
+"""
+import csv
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    """
+        Main Function of the program.
+    """
+
+    api_url = f'https://jsonplaceholder.typicode.com/'
+
+    user_id = int(argv[1])
+    user_data = requests.get(api_url + f'users/{user_id}').json()
+    user_tasks = requests.get(api_url + f'users/{user_id}/todos').json()
+
+    with open(f'{user_id}.csv', 'w', newline='') as f:
+
+        writer = csv.writer(f)
+        writer.writerow(["USER_ID",
+                         "USERNAME",
+                         "TASK_COMPLETED_STATUS",
+                         "TASK_TITLE"])
+
+        for task in user_tasks:
+            writer.writerow([user_id,
+                             user_data['name'],
+                             task['completed'],
+                             task['title']])
