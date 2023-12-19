b7e85c4f1e38ef619561f9259822276e895af7bd
Facusan2016 <facusan2016@gmail.com>
2023-12-19 15:37:29 -0300

    added exercise 1 to api/

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+
+"""
+    Program to gather data from an API.
+"""
+
+if __name__ == '__main__':
+    """
+        Main Function of the program.
+    """
+
+    api_url = f'https://jsonplaceholder.typicode.com/'
+
+    user_id = int(argv[1])
+    user_data = requests.get(api_url + f'users/{user_id}').json()
+    user_tasks = requests.get(api_url + f'users/{user_id}/todos').json()
+
+    completed_tasks = sum(1 for task in user_tasks if task['completed'])
+
+    print(f'Employee {user_data["name"]} is done with', end='')
+    print(f'tasks({completed_tasks}/{len(user_tasks)}):')
+
+    for task in user_tasks:
+        print(f'\t {task["title"]}')
---
+++ api/README.md
@@ -0,0 +1,1 @@
+In this section we're working with api' trying to understand the principles
